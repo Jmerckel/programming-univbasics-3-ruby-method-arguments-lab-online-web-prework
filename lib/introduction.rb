856f1390a050d307e28b90068a8e963a71bf306c
Jazmin M <jazminmerckel@gmail.com>
2019-07-31 03:16:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,8 +8,7 @@ introduction_with_language = (name, language)
 end
 introduction_with_language
 
-introduction_with_language_optional = 
-introduction_with_language = (name, language)
+introduction_with_language_optional = (name, language)
   puts "Hi, my name is #{Jaz} and I am learning to program in #{Ruby}"
 end
-introduction_with_language
+introduction_with_language (name, language)
